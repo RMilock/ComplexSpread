b1962e53657faeddb3191139c717510d6fee6e69
RMilock <riccardo.milocco@gmail.com>
2021-04-09 17:48:28 +0200

    stable_ver 18:00; todo: separate save G and sir

--- simple_sir_last_ver.py
+++ simple_sir_last_ver.py
@@ -20,31 +20,25 @@ I "round_half_up" since, as for N = 15, k = N/2 = 8, in many cases
 it drives to a nearer pruning.
 """
 
-##MASSIVE BUG: WS Model change D_odd into D_odd - 1. So, all has to be calculated according
-# to this change: if R_0... ; 
-# think on: if changing instantly k_prog but R0 would be much shifted for this or
-# save R_0 @ first and then make steps forward
+'rewire all the edges with a probability of p'
+N = int(30)
 
 def even_int(x):
   if int(x) % 2 != 0: return int(x-1)
   return int(x)
 
-'rewire all the edges with a probability of p'
-N = int(1e3)
-
-for pruning in [True, False]: #if 1 needed: add ``break``
+for pruning in [True]: #if 1 needed: add ``break``
 
   if pruning == True:
     p = 0.1#np.linspace(0,p_max,int(p_max*10)+1)
     print("I'm pruning!")
-    betas = [1e-3, 3e-3, ]
-    k_prog = [even_int(N/x) for x in \
-              [2**i for i in range(0,pow_max(N, num_iter = "all"))]] #if pow_max +1 --> error of connectivity: D = k_odd - 1
-    beta_prog = [ [ beta*N/k for k in k_prog[:len(set(k_prog))] ] for beta in betas ]
-    
+    betas = [2e-3]
+
     'In WS model, if D = odd, D = D - 1. So, convert it now'
-    mu_prog = np.linspace(0.016,1,25) 
-    #interesting behaviour for beta = 1e-3 and [0.467, 0.385, 0.631]
+    k_prog = [even_int(N/x) for x in \
+              [2**i for i in range(0,pow_max(N, num_iter = "all"))]]*len(betas) #if pow_max +1 --> error of connectivity: D = k_odd - 1
+    beta_prog = [beta*N/k for beta in betas for k in k_prog[:len(set(k_prog))]]
+    mu_prog = np.linspace(0.016,1,25) #[0.467, 0.385, 0.631]
     
     folder = "WS_Pruned" 
 
@@ -54,12 +48,10 @@ for pruning in [True, False]: #if 1 needed: add ``break``
 
     total_iterations = 0
     for mu in mu_prog:
-      for i in range(len(beta_prog)):
-        for beta, D in zip(beta_prog[i], k_prog):
+        for beta, D in zip(beta_prog, k_prog):
           #print("R0: ", beta*D/mu)
-          if  0 < beta*D/mu < 10 and beta <= 1:
-            print("R0 < 10", beta*D/mu)
-          else: print("beta %s, beta*D/mu %s" % (beta, beta*D/mu)); break
+          if  0 < beta*D/mu < 7 and beta <= 1:
+            total_iterations += 1
     print("Total Iterations:", total_iterations)
     
     done_iterations = 0
@@ -73,27 +65,28 @@ for pruning in [True, False]: #if 1 needed: add ``break``
             done_iterations+=1
             print("Iterations left: %s" % ( total_iterations - done_iterations ) )
             print("beta", beta, "D", D, "R0", beta*D/mu, "mu", mu) 
-            ws_sir(G, pruning = pruning, folder = folder, D = D, p = p, beta = beta, mu = mu, plot_all=False)
+            for adj_or_sir in [True, False]:
+              ws_sir(G, pruning = pruning, folder = folder, D = D, p = p, beta = beta, mu = mu, adj_or_sir=adj_or_sir)
 
   if pruning == False:
     'test != kind of '
     print("-----------------I'm NOT pruning!")
-    k_prog = np.arange(2,18,4)
-    p_prog = [0.1]#np.linspace(0,p_max,int(p_max*10)+1)
+    p_prog = [0,0.1]#np.linspace(0,p_max,int(p_max*10)+1)
     mu_prog = np.linspace(0.001,1,10)
-    beta_prog = np.linspace(0.001,1,10)
+    beta_prog = np.linspace(0.5,1,10)
+    k_prog = [2]*len(mu_prog)#np.arange(2,18,4)
     folder = "WS_Epids"
 
     total_iterations = 0
-    for beta, D, mu in product(beta_prog, k_prog, mu_prog):
+    for beta, D, mu, p in product(beta_prog, k_prog, mu_prog, p_prog):
         if  .3 < beta*D/mu < 16:
           total_iterations += 1
     print("Total Iterations:", total_iterations)
     done_iterations = 0
-    for D in k_prog:
+    for D, p in product(k_prog, p_prog):
       G = nx.connected_watts_strogatz_graph( n = N, k = D, p = p, seed = 1 ) #k is the number of near linked nodes
-      for mu,p,beta in product(mu_prog, p_prog, beta_prog): 
-        if  0.3 < beta*D/mu < 16:
+      for mu, beta in product(mu_prog, beta_prog): 
+        if  0.3 < beta*D/mu < 16:   
           done_iterations+=1
           print("Iterations left: %s" % ( total_iterations - done_iterations ) )
-          ws_sir(G, folder = folder, pruning = pruning, D = D, p = p, beta = beta, mu = mu, plot_all=False)
+          ws_sir(G, folder = folder, pruning = pruning, D = D, p = p, beta = beta, mu = mu, adj_or_sir=False)
