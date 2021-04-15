13ef73d268235857bd510beb59e46620c12819db
RMilock <riccardo.milocco@gmail.com>
2021-04-15 13:41:49 +0200

    added p_prog and better visualization of process via terminal

--- simple_sir_last_ver.py
+++ simple_sir_last_ver.py
@@ -21,16 +21,17 @@ it drives to a nearer pruning.
 """
 
 'rewire all the edges with a probability of p'
-N = int(30)
+N = int(1e3)
 
 def even_int(x):
   if int(x) % 2 != 0: return int(x-1)
   return int(x)
 
-for pruning in [True]: #if 1 needed: add ``break``
+for pruning in [True, False]: #if 1 needed: add ``break``
 
   if pruning == True:
-    p = 0.1#np.linspace(0,p_max,int(p_max*10)+1)
+    p_max = .2
+    p_prog = np.linspace(0,p_max,int(p_max*10)+1)
     print("I'm pruning!")
     betas = [2e-3]
 
@@ -47,7 +48,7 @@ for pruning in [True]: #if 1 needed: add ``break``
     "\nmu_prog %s, len: %s" % (mu_prog, len(mu_prog)), )
 
     total_iterations = 0
-    for mu in mu_prog:
+    for mu, p in product(mu_prog, p_prog):
         for beta, D in zip(beta_prog, k_prog):
           #print("R0: ", beta*D/mu)
           if  0 < beta*D/mu < 7 and beta <= 1:
@@ -61,33 +62,34 @@ for pruning in [True]: #if 1 needed: add ``break``
       #print("beta %s ; mu: %s; beta_1.2: %s" % (beta, mu, beta) )
       'With p = 1 and <k>/N ~ 0, degree distr is sim to a Poissonian'
       G = nx.connected_watts_strogatz_graph( n = N, k = D, p = p, seed = 1 ) #k is the number of near linked nodes
-      for mu in mu_prog: 
+      for mu, p in product(mu_prog, p_prog): 
           if 0 < beta*D/mu < 7 and beta <= 1:
             done_iterations+=1
             print("Iterations left: %s" % ( total_iterations - done_iterations ) )
-            print("beta", beta, "D", D, "R0", beta*D/mu, "mu", mu) 
+            #print("beta", beta, "D", D, "R0", beta*D/mu, "mu", mu) 
             ws_sir(G, saved_nets=saved_nets, pruning = pruning, folder = folder, D = D, p = p, beta = beta, mu = mu)
 
   if pruning == False:
     'test != kind of '
     print("-----------------I'm NOT pruning!")
-    p_prog = [0,0.1]#np.linspace(0,p_max,int(p_max*10)+1)
+    p_prog = [0.1]#np.linspace(0,p_max,int(p_max*10)+1)
     mu_prog = np.linspace(0.001,1,10)
-    beta_prog = np.linspace(0.5,1,10)
-    k_prog = [2]*len(mu_prog)#np.arange(2,18,4)
-    folder = "WS_Epids"
+    beta_prog = np.linspace(0.7,1,10)
+    k_prog = [2]#np.arange(2,18,4)
+    folder = "WS_Epids_D2"
 
     total_iterations = 0
     for beta, D, mu, p in product(beta_prog, k_prog, mu_prog, p_prog):
-        if  .3 < beta*D/mu < 16:
+        if  .3 < beta*D/mu < 8:
           total_iterations += 1
     print("Total Iterations:", total_iterations)
     done_iterations = 0
+    
     saved_nets = []
     for D, p in product(k_prog, p_prog):
       G = nx.connected_watts_strogatz_graph( n = N, k = D, p = p, seed = 1 ) #k is the number of near linked nodes
       for mu, beta in product(mu_prog, beta_prog): 
-        if  0.3 < beta*D/mu < 16:   
+        if  0.3 < beta*D/mu < 8:   
           done_iterations+=1
           print("Iterations left: %s" % ( total_iterations - done_iterations ) )
           ws_sir(G, saved_nets = saved_nets, folder = folder, pruning = pruning, D = D, p = p, beta = beta, mu = mu)
