125ff8d4d249a0963ffd7b81520a144f62f33da1
RMilock <riccardo.milocco@gmail.com>
2021-04-17 17:36:48 +0200

    added R0_min,_max , save only NNR_Conf_Model, save net before sir

--- Conf_Model_SIR.py
+++ Conf_Model_SIR.py
@@ -6,16 +6,14 @@ import networkx as nx
 # %matplotlib inline
 from itertools import product
 import os #to create a folder
-from definitions import rhu, plot_save_net, plot_save_sir, config_pois_model, NN_pois_net, pois_pos_degrees
+from definitions import my_dir, save_log_file, rhu, plot_save_net, plot_save_sir, config_pois_model, NN_pois_net, pois_pos_degrees
 
 'save scaled version for better visualization'
 def scaled_conf_pois(G,D,cut_off=30):    
   scaled_N = int(G.number_of_nodes()/cut_off) # int(D/cut_off)
-  #if int(D) >= 2: 
-  #  print("The rescaled one has N: %s and D: %s" % (int(N/cut_off), int(D/cut_off)) )
   return config_pois_model(scaled_N, D)
 
-"""## Configurational Model with poissonian degree:
+'''Configurational Model with poissonian degree:
 1) Question: 
   1. using this meth, loops and parallel edges are present leading 
   to a adj_matrix not normalized to 1. Since neighbors are involved in sir 
@@ -24,56 +22,46 @@ def scaled_conf_pois(G,D,cut_off=30):
   So, I prefer to remove them, but I loose a lot of precision if $D / N !<< 1$. 
   <br>Ex., $D = 50 = N, <k> ~ 28$. For $N= 1000 \text{ and } D = 3 \textrm{ or } 8, 
   <k> \textrm{is acceptable.}$
-  2. To avoid the picking of $0$, choice on a poisson sequence
-
-TODO: implement the idea of a pruning factor as in ws_sir
-"""
-
-p_max = 0; N = int(1e3)
+'''
+p_max = 0; N = int(40)
 
 'progression of net-parameters'
-k_prog = np.arange(10,36,2)
+k_prog = np.arange(2,10,2)
 p_prog = np.linspace(0,p_max,int(p_max*10)+1)
-mu_prog = np.linspace(0.001,1,15)
-beta_prog = np.linspace(0.001,1,15)
+mu_prog = np.linspace(0.01,1,15)
+beta_prog = np.linspace(0.01,1,15)
 p_prog = [0]
-R0_max = 5; R_min = 0.3
+R0_min = 0.5; R0_max = 16
+
 
 'try only with p = 0.1'
 total_iterations = 0
 for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
-  if R_min < beta*D/mu < R0_max:
+  if R0_min < beta*D/mu < R0_max:
     total_iterations+=1
 print("Total Iterations:", total_iterations)
 done_iterations = 0
 
 saved_nets = []
 for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
-  if R_min < beta*D/mu < R0_max:
+  if R0_min < beta*D/mu < R0_max:
     done_iterations+=1
     print("Iterations left: %s" % ( total_iterations - done_iterations ) )
 
     folder = "Config_Model"
     G = config_pois_model(N, D)
-  
-    
-    scaled_G = scaled_conf_pois(G, D = D)
-    
     'plot G, degree distribution and the adiaciency matrix and save them'
-    #Config_SIR def: D = 8, beta, mu = 0.1, 0.05
-    #plot_save_net(G, scaled_G = scaled_G, folder = folder, D = D, p = 0)
-    #plot_save_sir(G, folder = folder, beta = beta, D = D, mu = mu, p = 0)
-
-    #infos_sorted_nodes(G, True)
     G = NN_pois_net(G, D = D)
+    
     folder = "NNR_Conf_Model"
-
-    plot_save_sir(G, folder = folder, beta = beta, D = D, mu = mu, p = p_max)
-
-    'possibly save scaled version for better visualization'
-    scaled_G = NN_pois_net(G = scaled_G, D = D)
+    text = "N %s;\n k_prog %s, len: %s;\np_prog %s, len: %s;\nbeta_prog %s, len: %s;\nmu_prog %s, len: %s;\nR0_min %s, R0_max %s\n---\n" \
+            % (N, k_prog, len(k_prog), beta_prog, len(beta_prog), \
+            mu_prog, len(mu_prog), p_prog, len(p_prog), R0_min, R0_max)
 
     if "N%s_D%s_p%s"% (N,D,rhu(p,3)) not in saved_nets: 
       plot_save_net(G = G, scaled_G = G, folder = folder, D = D, p = p)
       saved_nets.append("N%s_D%s_p%s"% (N,D,rhu(p,3)))
       print(saved_nets)
+    plot_save_sir(G, folder = folder, beta = beta, D = D, mu = mu, p = p_max)
+
+    save_log_file(folder = my_dir() + folder, text = text)
