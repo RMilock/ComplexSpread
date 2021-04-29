a17760bad53827ec5111c4902a245a7e7752e1f4
RMilock <riccardo.milocco@gmail.com>
2021-04-29 23:33:13 +0200

    update

--- playgr.py
+++ playgr.py
@@ -1,18 +1,9 @@
 import numpy as np
-from itertools import product
 
+b = list( range(30) )
+total = len(b)
 
-D = np.arange(2,16,2)
-p_prog = np.concatenate((np.array([0.001]), np.linspace(0.012,1,9)))
+f = lambda x: x/total
+quotient_b = list(map(f, b))
 
-print( list([(D[i],p_prog*D[i]) for i in range(len(D))]))
-
-count = 0
-for d, p in product(D,p_prog):
-    D_pruned = D[D!=d]
-    print(d,p,D_pruned)
-    p_pruned = p_prog[p_prog!=p]
-    for d2, p2 in product(D_pruned, p_pruned):
-        if d*p == d2*p2: count+=1; print("This is equal", d*p)
-
-print("There're %s equals values" % count)
+print(quotient_b)
