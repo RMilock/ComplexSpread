e0656eb5d1358a95c650e92ac3e3f7f7ac7ea3d9
RMilock <riccardo.milocco@gmail.com>
2021-04-29 22:20:01 +0200

    added ring or rnd relink + std program main

--- community_by_color.py
+++ community_by_color.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
-from definitions import plot_G_degdist_adjmat_sir
+from definitions import save_log_params, plot_sir, check_loops_parallel_edges, remove_loops_parallel_edges
 
 
 NODE_LAYOUT = nx.circular_layout
@@ -143,43 +143,95 @@ def _get_random_point_on_a_circle(origin, radius):
     y = y0 + radius * np.sin(random_angle)
     return np.array([x, y])
 
+def comm_caveman_relink(cliques = 8, clique_size = 7, p = 0,  relink_rnd = 0, relink_ring = 0):
+    import numpy as np
+    import numpy.random as npr
+    'caveman_graph'
+    G = nx.caveman_graph(l = cliques, k = clique_size)
 
-def test(cliques = 8, clique_size = 7):
-    # create test data
-    g = nx.connected_caveman_graph(l = cliques, k = clique_size)
-    partition = {ii : np.int(ii/clique_size) for ii in range(cliques * clique_size)}
+    'relink nodes to neighbor "cave"'
+    total_nodes = clique_size*cliques
+    #if relink_ring != 0: 
+    for clique in range(cliques):
+        nodes_inclique = np.arange(D*(clique), D*(1+clique))
+        if relink_ring != 0:
+            tests = npr.choice(nodes_inclique, relink_ring)
+            attached_nodes = npr.choice( np.arange(D*(1+clique), D*(2+clique)), 
+                                        size = len(tests) )
+            attached_nodes = attached_nodes % np.max((total_nodes,1))
+            for test, att_node in zip(tests, attached_nodes):
+                #print("NN - clique add:", (test,att_node))
+                G.add_edge(test,att_node)
+        if p != 0:
+            tests = npr.choice(nodes_inclique, relink_rnd)
+            attached_nodes = npr.choice([x for x in G.nodes() if x not in nodes_inclique], 
+                                        size = len(tests))
+            for test, att_node in zip(tests, attached_nodes):
+                #print("relink", (test,att_node))
+                if npr.uniform() < p: G.add_edge(test,att_node)
 
-    pos = partition_layout(g, partition, ratio=0.2)
-    nx.draw(g, pos, node_color=list(partition.values()))
-    return g
+    check_loops_parallel_edges(G)
+    remove_loops_parallel_edges(G)
+    #check_loops_parallel_edges(G)
 
-def test2():
+    print("size/cliq: %s, cliq/size: %s" % (clique_size/cliques, cliques/clique_size) )
 
-    # create test data
-    cliques = 8
-    clique_size = 7
-    g = nx.connected_caveman_graph(cliques, clique_size)
-    partition = {ii : np.int(ii/clique_size) for ii in range(cliques * clique_size)}
-
-    # add additional between-clique edges
-    total_nodes = cliques*clique_size
-    for ii in range(cliques):
-        start = ii*clique_size + int(clique_size/2)
-        stop = (ii+cliques/2)*clique_size % total_nodes + int(clique_size/2)
-        g.add_edge(start, stop)
-
-    pos = partition_layout(g, partition, ratio=0.2)
-    nx.draw(g, pos, node_color=list(partition.values()))
-    plt.show()
+    
+    return G
 
 
-'''if __name__ == '__main__':
-    test()
-    test2()'''
+'start of the main()'
+from itertools import product
+from definitions import rhu, plot_save_net, plot_save_sir
 
-beta = 0.01; mu = 0.16; N = int(1e3); D = 30; numb_iter = 500
-num_clique = int(N/D)
-G = test(cliques=num_clique, clique_size=D)
 
-plot_G_degdist_adjmat_sir(G, D = D, beta = beta, mu = mu, numb_iter=numb_iter) #mf! expected
-plt.show()
+p_max = 0.1; N = int(1e3)
+
+'progression of net-parameters'
+k_prog = np.arange(10,32,2)
+p_prog = np.linspace(0,p_max,4)
+mu_prog = np.linspace(0.01,1,7)
+beta_prog = np.linspace(0.01,1,7)
+R0_min = 0.5; R0_max = 7
+
+N = int(1e3)
+
+'try only with p = 0.1'
+total_iterations = 0
+for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
+  if R0_min < beta*D/mu < R0_max:
+    total_iterations+=1
+print("Total Iterations:", total_iterations)
+done_iterations = 0
+
+saved_nets = []
+for D,p,beta,mu in product(k_prog, p_prog, beta_prog, mu_prog):  
+  if R0_min < beta*D/mu < R0_max:
+    done_iterations+=1
+    print("Iterations left: %s" % ( total_iterations - done_iterations ) )
+    
+    folder = "Caveman_Model"
+    clique_size = D; cliques = int(N/D)
+    G = comm_caveman_relink(cliques=cliques, clique_size = D, 
+                            p = p, relink_rnd = 1, relink_ring = 1)
+    
+    partition = {node : np.int(node/clique_size) for node in range(cliques * clique_size)}
+    pos = partition_layout(G, partition, ratio=clique_size/cliques*0.1)
+
+    text = "N %s;\n k_prog %s, len: %s;\np_prog %s, len: %s;\nbeta_prog %s, len: %s;\nmu_prog %s, len: %s;\nR0_min %s, R0_max %s\n---\n" \
+            % (N, k_prog, len(k_prog), p_prog, len(p_prog), beta_prog, len(beta_prog), \
+            mu_prog, len(mu_prog),  R0_min, R0_max)
+
+    if "N%s_D%s_p%s"% (N,D,rhu(p,3)) not in saved_nets: 
+      print("This is p", p)
+      plot_save_net(G = G, pos = pos, partition = partition, folder = folder, D = D, p = p, done_iterations = done_iterations)
+      saved_nets.append("N%s_D%s_p%s"% (N,D,rhu(p,3)))
+      print(saved_nets)
+    plot_save_sir(G, folder = folder, beta = beta, D = D, mu = mu, p = p_max, done_iterations = done_iterations)
+    
+    save_log_params(folder = folder, text = text)
+
+'''
+ax = plt.subplot()
+plot_sir(G, ax = ax, D = D, beta = beta, mu = mu) #mf! expected
+plt.show()'''
