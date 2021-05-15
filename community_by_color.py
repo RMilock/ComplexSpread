039b767dc990cbe21e12fbac14f12dfbd1f52de3
RMilock <riccardo.milocco@gmail.com>
2021-05-15 23:26:26 +0200

    add plot_save_nes, num_rl_ring

--- community_by_color.py
+++ community_by_color.py
@@ -9,55 +9,66 @@ caveman_defs
     
 'start of the main()'
 from itertools import product
-from definitions import rhu, plot_save_net, plot_save_sir
+from definitions import rhu, plot_save_nes
 
 partition_layout, comm_caveman_relink = caveman_defs()
    
 p_max = 0.1; N = int(1e3)
 
 'progression of net-parameters'
-k_prog = np.arange(2,34,2)
-p_prog = np.linspace(0.1,p_max,4)
-mu_prog = np.linspace(0.01,1,7)
-beta_prog = np.linspace(0.01,1,7)
-R0_min = 0.5; R0_max = 10
+k_prog = np.arange(1,11,2) #https://www.prb.org/about/ -> Europe householdsize = 3
+p_prog = np.linspace(0,p_max,2)
+mu_prog = np.linspace(0.001,1,7)
+beta_prog = np.linspace(0.001,1,7)
+R0_min = 0.5; R0_max = 5
+folder = "Caveman_Model"
 
 'try only with p = 0.1'
 total_iterations = 0
 for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
-  if R0_min < beta*D/mu < R0_max:
-    total_iterations+=1
+  for n_rl_ring in [rhu(x) for x in np.linspace(1,D,3)]:
+    if R0_min < beta*D/mu < R0_max:
+      total_iterations+=1
 print("Total Iterations:", total_iterations)
 done_iterations = 0
 
 saved_nets = []
 for D,p,beta,mu in product(k_prog, p_prog, beta_prog, mu_prog):  
-  if R0_min < beta*D/mu < R0_max:
-    done_iterations+=1
-    print("Iterations left: %s" % ( total_iterations - done_iterations ) )
-    
-    folder = "Caveman_Model"
-    clique_size = D; cliques = int(N/D)
-    G = comm_caveman_relink(cliques=cliques, clique_size = D, 
-                            p = p, relink_rnd = D, numb_rel_inring = 1)
-    
-    partition = {node : np.int(node/clique_size) for node in range(cliques * clique_size)}
-    pos = partition_layout(G, partition, ratio=clique_size/cliques*0.1)
+  for n_rl_ring in [rhu(x) for x in np.linspace(1,D,3)]:
+    if R0_min < beta*D/mu < R0_max:
+      done_iterations+=1
+      print("Iterations left: %s" % ( total_iterations - done_iterations ) )
+      clique_size = D; cliques = int(N/D)
+      text = "N %s;\nk_prog %s, len: %s;\np_prog %s, len: %s;\nbeta_prog %s, len: %s;\nmu_prog %s, len: %s;\nR0_min %s, R0_max %s; \nTotal Iterations: %s;\n---\n" \
+              % (N, k_prog, len(k_prog), p_prog, len(p_prog), beta_prog, len(beta_prog), \
+              mu_prog, len(mu_prog),  R0_min, R0_max, total_iterations)
 
-    text = "N %s;\n k_prog %s, len: %s;\np_prog %s, len: %s;\nbeta_prog %s, len: %s;\nmu_prog %s, len: %s;\nR0_min %s, R0_max %s\n---\n" \
-            % (N, k_prog, len(k_prog), p_prog, len(p_prog), beta_prog, len(beta_prog), \
-            mu_prog, len(mu_prog),  R0_min, R0_max)
+      save_log_params(folder = folder, text = text, done_iterations = done_iterations)
 
+      
+      G = comm_caveman_relink(cliques=cliques, clique_size = D, 
+                              p = p, relink_rnd = D, numb_rel_inring = 1)
+      
+      partition = {node : np.int(node/clique_size) for node in range(cliques * clique_size)}
+      pos = partition_layout(G, partition, ratio=clique_size/cliques*0.1)
+
+      plot_save_nes(G = comm_caveman_relink(cliques=cliques, clique_size = D, p = p, relink_rnd = D, numb_rel_inring = 1),
+      pos = pos, partition = partition, p = p, folder = folder, adj_or_sir="AdjMat", done_iterations=done_iterations)
+
+      'diff VS plot_save_nes: "SIR", no pos, no partition'
+      plot_save_nes(G = comm_caveman_relink(cliques=cliques, clique_size = D, 
+                              p = p, relink_rnd = D, numb_rel_inring = 1),
+      p = p, folder = folder, adj_or_sir="SIR", beta = beta, mu = mu, done_iterations=done_iterations)
+
+    '''
     if "N%s_D%s_p%s"% (N,D,rhu(p,3)) not in saved_nets: 
       print("This is p", p)
-      plot_save_net(G = G, pos = pos, partition = partition, folder = folder, D = D, p = p, done_iterations = done_iterations)
+      plot_save_net(G = G,  folder = folder, D = D, p = p, done_iterations = done_iterations)
       saved_nets.append("N%s_D%s_p%s"% (N,D,rhu(p,3)))
       print(saved_nets)
     plot_save_sir(G, folder = folder, beta = beta, D = D, mu = mu, p = p, done_iterations = done_iterations)
     
-    save_log_params(folder = folder, text = text)
 
-'''
 ax = plt.subplot()
 plot_sir(G, ax = ax, D = D, beta = beta, mu = mu) #mf! expected
 plt.show()'''
