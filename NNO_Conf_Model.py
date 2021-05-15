6c49f28f06ee3e2478bc5156435f18f1e5cea00d
RMilock <riccardo.milocco@gmail.com>
2021-05-15 23:29:53 +0200

    mv outside the prams_text

--- NNO_Conf_Model.py
+++ NNO_Conf_Model.py
@@ -15,25 +15,23 @@ folder = f"Overlapping_Rew_Add_{add_edges_only}"
 
 'try only with p = 0.1 -- since NN_Overl_add_edge augment D, we are overestimating tot_number'
 total_iterations = 0
-for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
+for D,p,beta,mu in product(k_prog, p_prog, beta_prog, mu_prog):  
   if R0_min < beta*D/mu < R0_max:
     total_iterations+=1
 print("Total Iterations:", total_iterations)
 done_iterations = 0
 
+'save parameters'
+text = "N %s;\nk_prog %s, len: %s;\np_prog %s, len: %s;\nbeta_prog %s, len: %s;\nmu_prog %s, len: %s;\nR0_min %s, R0_max %s; \nTotal Iterations: %s;\n---\n" \
+        % (N, k_prog, len(k_prog), p_prog, len(p_prog), beta_prog, len(beta_prog), \
+        mu_prog, len(mu_prog),  R0_min, R0_max, total_iterations)
+save_log_params(folder = folder, text = text)
+
 saved_nets = []
-for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
+for D,p,beta,mu in product(k_prog, p_prog, beta_prog, mu_prog,):  
   if R0_min < beta*D/mu < R0_max:
-
     done_iterations+=1
-    print("\nIterations left: %s" % ( total_iterations - done_iterations ) )
-       
-    'save parameters'
-    text = "N %s;\n k_prog %s, len: %s;\np_prog %s, len: %s;\nbeta_prog %s, len: %s;\nmu_prog %s, len: %s;\nR0_min %s, R0_max %s\n---\n" \
-            % (N, k_prog, len(k_prog), p_prog, len(p_prog), beta_prog, len(beta_prog), \
-            mu_prog, len(mu_prog),  R0_min, R0_max)
-    save_log_params(folder = folder, text = text, done_iterations = done_iterations)
-
+    print("\nIterations left: %s" % ( total_iterations - done_iterations ) )  
     plot_save_nes(G = NN_Overl_pois_net(N, D, p = p, add_edges_only = add_edges_only), 
     p = p, folder = folder, adj_or_sir="AdjMat", done_iterations=done_iterations)
     plot_save_nes(G = NN_Overl_pois_net(N, D, p = p, add_edges_only = add_edges_only), 
