b3f269509121b62cd0c16e15ba5c2eec278bf01d
RMilock <riccardo.milocco@gmail.com>
2021-06-03 23:13:35 +0200

    add comments in sir + \bar{R}_net for all scen., comments on itermean sir, plot beta/mu not R0 folder

--- definitions.py
+++ definitions.py
@@ -7,6 +7,7 @@ import os #to create a folder
 #Thurner pmts: beta = 0.1, mu = 0.16; D = 3 vel 8
 #MF def: beta, mu = 0.001/cf, 0.05/cf or 0.16/cf ; cf = 1
 
+#this has been saved in morning of the 3.6.2021
 
 def my_dir():
   #return "/content/drive/MyDrive/Colab_Notebooks/Thesis/Complex_Plots/"
@@ -117,32 +118,30 @@ def sir(G, mf = False, beta = 1e-3, mu = 0.05, start_inf = 10, seed = False):
   'start and continue whenever there s 1 infected'
   while(len(inf_list)>0):        
     daily_new_inf = 0
-    
     'Infection Phase: inf_list = prev_time infecteds'
     'each infected tries to infect all of the neighbors'
     for i in inf_list:
-      'Select the neighbors of the infected node'
-      if not mf: tests = G.neighbors(i) #only != wrt to the SIS: contact are taken from G.neighbors            
-      if mf: 
-        ls = list(range(N)); ls.remove(i)
-        tests = random.choices(ls, k = int(mean)) #spread very fast since multiple infected center
-      tests = [int(x) for x in tests] #convert 35.0 into int
-      for j in tests:
-        'If the contact is susceptible and not infected by another node in the future_state, try to infect it'
-        if current_state[j] == 'S' and future_state[j] == 'S':
-          if random.random() < beta:
-            future_state[j] = 'I'; daily_new_inf += 1 
-
-          else:
-            future_state[j] = 'S'
+        'Select the neighbors of the infected node'
+        if not mf: tests = G.neighbors(i) #only != wrt to the SIS: contact are taken from G.neighbors            
+        if mf: 
+          ls = list(range(N)); ls.remove(i)
+          tests = random.choices(ls, k = int(mean)) #spread very fast since multiple infected center
+        tests = [int(x) for x in tests] #convert 35.0 into int
+        for j in tests:
+            'If the contact is susceptible and not infected by another node in the future_state, try to infect it'
+            if current_state[j] == 'S' and future_state[j] == 'S':
+                if random.random() < beta:
+                    future_state[j] = 'I'; daily_new_inf += 1
+                else:
+                    future_state[j] = 'S'
     
     'Recovery Phase: only the prev inf nodes (=inf_list) recovers with probability mu'
     'not the new infected'        
     for i in inf_list:
-      if random.random() < mu:
-        future_state[i] = 'R'
-      else:
-        future_state[i] = 'I'
+        if random.random() < mu:
+            future_state[i] = 'R'
+        else:
+            future_state[i] = 'I'
     
     'Time update: once infections and recovery ended, we move to the next time-step'
     'The future state becomes the current one'
@@ -155,20 +154,13 @@ def sir(G, mf = False, beta = 1e-3, mu = 0.05, start_inf = 10, seed = False):
     'Saves the fraction of infected and recovered in the current time-step'
     prevalence.append(len(inf_list)/float(N))
     recovered.append(len(rec_list)/float(N))
-    cum_prevalence.append(cum_prevalence[-1]+daily_new_inf)  
-    
-    '''def diff_prev(): 
-      diff = rhu(prevalence[-1],3)-rhu(prevalence[-2],3)
-      if diff > 0: return diff
-      return 0
+    cum_prevalence.append(cum_prevalence[-1]+daily_new_inf/N)  
+    #loop +=1;
+    #print("loop:", loop, cum_total_inf, cum_total_inf[-1], daily_new_inf/N)
 
-    if not np.isclose( daily_new_inf/N, diff_prev() ):
-      print("Not close:: daily_new_cases/N == prevalence[-1]", prevalence[-4:], daily_new_inf/N, 
-      prevalence[-1], prevalence[-2], diff_prev(), rhu(diff_prev(),3))'''
-    
     num_susc.append(N*(1 - prevalence[-1] - recovered[-1]))
     #print("\nnum_susc, prevalence, recovered",num_susc, prevalence, recovered, 
-    # len(num_susc), len(prevalence), len(recovered))
+    #len(num_susc), len(prevalence), len(recovered))
   
   degrees = [j for i,j in G.degree()]
   sum_degrees = np.sum(degrees)
@@ -203,7 +195,7 @@ def itermean_sir(G, mf = False, numb_iter = 200, beta = 1e-3, mu = 0.05, start_i
     avg_R, prev, rec, cum_prev = sir(G, beta = beta, mu = mu, start_inf = start_inf, mf = mf)
     tmp_traj = prev, rec, cum_prev
     #print("\n avg_R %s, ", avg_R)
-    itermean_R += (avg_R) / max(1,numb_iter)
+    itermean_R += avg_R / max(1,numb_iter)
     if i % 50 == 0: 
       time_1sir = dt.datetime.now()-sir_start_time
       print("The time for 1 sir is", time_1sir)
@@ -272,8 +264,7 @@ def plot_sir(G, ax, folder = None, beta = 1e-3, mu = 0.05, start_inf = 10, numb_
   itermean_R_net, trajectories, avg = itermean_sir(G, mf = False, beta = beta, mu = mu, start_inf  = start_inf, numb_iter=numb_iter)
   print("\nMF-SIR loading...")
   itermean_R_mf, mf_trajectories, mf_avg = itermean_sir(G, mf = True, mu = mu, beta = beta, start_inf = start_inf, numb_iter = numb_iter)
-  print("itermean_R_net %s VS itermean_R_mf: %s" %(itermean_R_net, itermean_R_mf))
-  
+
   'plotting the many realisations'    
   colors = ["paleturquoise","wheat","lightgreen", "thistle"]
   
@@ -542,8 +533,8 @@ def plot_save_sir(G, folder, done_iterations = 1, p = 0, beta = 0.001, mu = 0.16
       right=0.992,
       hspace=0.2,
       wspace=0.2)
-      plt.suptitle(r"$R_0:%s, \bar{R}_{net}:%s, N:%s, D:%s, N_{3-out}: %s, p:%s, \beta:%s, \mu:%s$"
-      % (rhu(R0,3),rhu(itermean_R_net,3),N,rhuD2, count_outsiders, rhu(p,3), rhu(beta,3), rhu(mu,3), ))
+      plt.suptitle(r"$R_0:%s, \bar{R}_{net}:%s, D_{1000}:%s, N_{3-out}: %s, p:%s, \beta:%s, \mu:%s$"
+      % (rhu(R0,3),rhu(itermean_R_net,3), rhuD2, count_outsiders, rhu(p,3), rhu(beta,3), rhu(mu,3), ))
       #plt.show()
       plt.savefig( file_path )
       print("time 1_plot_save_sir:", dt.datetime.now()-start_time) 
@@ -1148,3 +1139,4 @@ def bam(N,m,N0):
   'returns the graph'
 
   return G
+
