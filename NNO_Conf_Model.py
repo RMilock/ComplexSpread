c2d636316c009c9da7831bc834af792a62350b74
RMilock <riccardo.milocco@gmail.com>
2021-06-11 22:34:43 +0200

    stable version. todo: erase all not present in the "main" definitions.py

--- NNO_Conf_Model.py
+++ NNO_Conf_Model.py
@@ -1,10 +1,12 @@
 import numpy as np
 from itertools import product
-from definitions import NN_Overl_pois_net, save_log_params, plot_save_nes, \
-  parameters_net_and_sir, NestedDict, my_dir
+from definitions import NNOverl_pois_net, save_log_params, plot_save_nes, \
+  parameters_net_and_sir, NestedDict, my_dir, jsonKeys2int
+import os; import json
 import matplotlib.pylab as plt
 
 N = int(1e3); add_edges_only = True
+
 
 'progression of net-parameters'
 '''
@@ -16,9 +18,13 @@ mu_prog = np.linspace(0.01,1,8)
 R0_min = 0; R0_max = 30'''
 
 
-folder = f"Overlapping_Rew_Add_{add_edges_only}"
-std_pmbD_dic = NestedDict()
-my_dir = my_dir()
+folder = f"NNO_Conf_Model_addE_{add_edges_only}" #add edges instead of rew
+ordp_pmbD_dic = NestedDict()
+ordp_path = "".join( (my_dir(),folder,"/OrdParam/saved_ordp_dict.txt") )
+if os.path.exists(ordp_path): 
+  with open(ordp_path,"r") as f:
+    ordp_pmbD_dic = json.loads(f.read(), object_hook=jsonKeys2int)
+
 k_prog, p_prog, beta_prog, mu_prog, R0_min, R0_max =  \
   parameters_net_and_sir(folder = folder) 
 
@@ -41,29 +47,34 @@ for D,p,beta,mu in product(k_prog, p_prog, beta_prog, mu_prog,):
   if R0_min <= beta*D/mu <= R0_max:
     done_iterations+=1
     print("\nIterations left: %s" % ( total_iterations - done_iterations ) )  
-    plot_save_nes(G = NN_Overl_pois_net(N, D, p = p, add_edges_only = add_edges_only), 
+    if os.path.exists(ordp_path): 
+      with open(ordp_path,"r") as f:
+        ordp_pmbD_dic = json.loads(f.read(), object_hook=jsonKeys2int)
+
+
+    plot_save_nes(G = NNOverl_pois_net(N, D, p = p, add_edges_only = add_edges_only), 
     p = p, folder = folder, adj_or_sir="AdjMat", done_iterations=done_iterations)
-    plot_save_nes(G = NN_Overl_pois_net(N, D, p = p, add_edges_only = add_edges_only), 
+    plot_save_nes(G = NNOverl_pois_net(N, D, p = p, add_edges_only = add_edges_only), 
     p = p, folder = folder, adj_or_sir="SIR", R0_max = R0_max, beta = beta, mu = mu, 
-    std_pmbD_dic = std_pmbD_dic, done_iterations=done_iterations)
+    ordp_pmbD_dic = ordp_pmbD_dic, done_iterations=done_iterations)
 
 '''
 import json as js
-print(js.dumps(std_pmbD_dic, sort_keys=False, indent=4))
+print(js.dumps(ordp_pmbD_dic, sort_keys=False, indent=4))
 
 for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
   'since D_real ~ 2*D (D here is fixing only the m and N0), R0_max-folder ~ 2*R0_max'
   if R0_min <= beta*D/mu <= R0_max: 
-    fixed_std = std_pmbD_dic[p][mu][beta]
-    print("std_pmbD_dic, p0, mu0, beta0, fixed_std", \
-      std_pmbD_dic, p, mu, beta, fixed_std)
+    fixed_std = ordp_pmbD_dic[p][mu][beta]
+    print("ordp_pmbD_dic, p0, mu0, beta0, fixed_std", \
+      ordp_pmbD_dic, p, mu, beta, fixed_std)
     x = sorted(fixed_std.keys())
     print("x", x)
     y = [fixed_std[i] for i in x]
     print("y", y)
     plt.plot(x,y,'-*')
-    std_path = my_dir + folder + "/Std/"
-    if not os.path.exists(std_path): os.makedirs(std_path)
-    plt.savefig("".join((std_path,"std_p%s_mu%s_beta%s.png" % (p,mu,beta))))
+    ordp_path = my_dir + folder + "/Std/"
+    if not os.path.exists(ordp_path): os.makedirs(ordp_path)
+    plt.savefig("".join((ordp_path,"std_p%s_mu%s_beta%s.png" % (p,mu,beta))))
     plt.close()
 '''
