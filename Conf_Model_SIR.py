5397ade1494ac5aa03ab9c8535caa055ffaeee37
RMilock <riccardo.milocco@gmail.com>
2021-09-03 18:16:43 +0200

    plot_save_nes <-> save_nes DELETED ALL THE OLD PMS (CF PREV VER)

--- Conf_Model_SIR.py
+++ Conf_Model_SIR.py
@@ -1,78 +1,17 @@
 import numpy as np
 from itertools import product
 import networkx as nx
-from definitions import main, save_log_params, plot_save_nes, \
+from definitions import main, save_log_params, save_nes, \
 NN_pois_net, parameters_net_and_sir, \
 NestedDict, my_dir, jsonKeys2int
 import os; import json
 
 '''Configurational Model with poissonian degree:
-1) Question: 
-  1. using this meth, loops and parallel edges are present leading 
-  to a adj_matrix not normalized to 1. Since neighbors are involved in sir 
-  and contagious is made by (fut = S, curr = S) - nodes, I may leave them, 
-  but there're not so in line of "social contacts". 
-  So, I prefer to remove them, but I loose a lot of precision if $D / N !<< 1$. 
-  <br>Ex., $D = 50 = N, <k> ~ 28$. For $N= 1000 \text{ and } D = 3 \textrm{ or } 8, 
-  <k> \textrm{is acceptable.}$
+Look for multi // and loops edges. Are affecting G.neighbors(i)? If so, remove them!
 '''
 N = int(10); folder = "NN_Conf_Model"
-
-'''
-ordp_pmbD_dic = NestedDict()
-std_path = "".join( (my_dir(),folder,"/Std/saved_std_dicts.txt") )
-if os.path.exists(std_path): 
-  with open(std_path,"r") as f:
-    ordp_pmbD_dic = json.loads(f.read(), object_hook=jsonKeys2int)
-
-'progression of net-parameters'
-
-k_prog = np.arange(2,34,2)
-p_prog = np.linspace(0,p_max,int(p_max*10)+1)
-mu_prog = np.linspace(0.1,1,15)
-beta_prog = np.linspace(0.1,1,15)
-p_prog = [0]
-R0_min = 0.5; R0_max = 6
-'''
 
 k_prog, p_prog, beta_prog, mu_prog, R0_min, R0_max \
   = parameters_net_and_sir(folder)
 main(folder = folder, N = N, k_prog = k_prog, p_prog = p_prog, \
   beta_prog = beta_prog, mu_prog = mu_prog, R0_min = R0_min, R0_max = R0_max)
-
-
-'''
-'try only with p = 0.1'
-total_iterations = 0
-for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
-  if R0_min <= beta*D/mu <= R0_max:
-    total_iterations+=1
-print("Total Iterations:", total_iterations)
-done_iterations = 0
-
-'save parameters'
-text = "N %s;\nk_prog %s, len: %s;\np_prog %s, len: %s;\nbeta_prog %s, len: %s;\nmu_prog %s, len: %s;\nR0_min %s, R0_max %s; \nTotal Iterations: %s;\n---\n" \
-        % (N, k_prog, len(k_prog), p_prog, len(p_prog), beta_prog, len(beta_prog), \
-        mu_prog, len(mu_prog),  R0_min, R0_max, total_iterations)
-save_log_params(folder = folder, text = text)
-
-saved_nets = []
-for D,mu,p,beta in product(k_prog, mu_prog, p_prog, beta_prog):  
-  if R0_min <= beta*D/mu <= R0_max:
-    done_iterations+=1
-    print("\nIterations left: %s" % ( total_iterations - done_iterations ) )
-    if os.path.exists(std_path): 
-      with open(std_path,"r") as f:
-        ordp_pmbD_dic = json.loads(f.read(), object_hook=jsonKeys2int)
-
-    G = NN_pois_net(N, folder = folder, ext_D = D, p = p, conn_flag = False)
-    print("connected components", len(list(nx.connected_components(G))))
-
-    plot_save_nes(G = G,
-    p = p, folder = folder, adj_or_sir="AdjMat", done_iterations=done_iterations)
-    plot_save_nes(G = G,
-    p = p, folder = folder, adj_or_sir="SIR", R0_max = R0_max, beta = beta, mu = mu, 
-    ordp_pmbD_dic = ordp_pmbD_dic, done_iterations=done_iterations)
-    print("---")
-
-'''
